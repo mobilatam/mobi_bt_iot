4b361e55c54ed69ba174e08a4bcbd473a17f0923
Diego Emerson Salvatierra Berrios <sbd0029992@est.univalle.edu>
2024-01-10 09:46:12 -0400

    Fix device manager interface remove asyn

--- lib/iot/interfaces/device_manager_interface.dart
+++ lib/iot/interfaces/device_manager_interface.dart
@@ -1,19 +1,19 @@
 abstract class DeviceManagerInterface {
   Future<void> getCkey({
     required String deviceKey,
-  }) async {}
+  });
 
   Future<void> unlockDevice({
     required int ckey,
-  }) async {}
+  });
 
   Future<void> lockDevice({
     required int ckey,
-  }) async {}
+  });
 
   Future<void> infoDevice({
     required int ckey,
-  }) async {}
+  });
 
-  Future<void> getUuidCommunication() async {}
+  Future<void> getUuidCommunication();
 }
