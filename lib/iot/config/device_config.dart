ae834e907ba8d2a454823ecb8b5e1cb3166bae30
Miguel <miguel@mobi.lat>
2024-11-05 09:22:29 -0300

    Update device_config.dart

--- lib/iot/config/device_config.dart
+++ lib/iot/config/device_config.dart
@@ -10,7 +10,7 @@ class DeviceConfig {
   String deviceMac = '';
 
   ///Todo: Hashing deviceUniqueKey or coming from environment variable
-  String deviceUniqueKey = 'yOTmK50z';
+  String deviceUniqueKey = 'N30671r3';
   int deviceCkey = 0;
   List<int> deviceInfo = [];
   List<String> deviceListUid = [];
